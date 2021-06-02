56a60df94602728f5e03a76134577045d7cbc247
mdavis8011 <84164593+mdavis8011@users.noreply.github.com>
2021-06-02 19:31:31 -0400

    Update photoFinder.py

--- photoFinder.py
+++ photoFinder.py
@@ -1,12 +1,12 @@
 import requests
-response = requests.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=34b062421be395f7de0912fe00a37193&per_page=500&format=json&nojsoncallback=1&tags=forest&extras=count_faves&license=7,8,9,10")
+response = requests.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=34b062421be395f7de0912fe00a37193&per_page=500&format=json&nojsoncallback=1&tags=forest%2C+trees&extras=count_faves&license=7,8,9,10&media=photos&content_type=4")
 
 pages = response.json()["photos"]["pages"]
 
 print(pages)
 
 for x in range(pages):
-    response = requests.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=34b062421be395f7de0912fe00a37193&per_page=500&format=json&nojsoncallback=1&tags=forest&extras=count_faves&license=7,8,9,10&page=%d"%(pages))
+    response = requests.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=34b062421be395f7de0912fe00a37193&per_page=500&format=json&nojsoncallback=1&tags=forest%2C+trees&extras=count_faves&license=7,8,9,10&media=photos&content_type=4")
     for photo in response.json()["photos"]["photo"]:
         if(int(photo["count_faves"]) >= 1):
             print(photo)
