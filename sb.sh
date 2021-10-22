b5fe43392ac41f54e758c527aa0d318ac56fe07b
Arash Golchubian <agolchub@fau.edu>
2021-10-22 09:41:30 -0700

    slurm script needs to install packages

--- sb.sh
+++ sb.sh
@@ -6,4 +6,4 @@
 # Load modules, if needed, run staging tasks, etc... Koko Software Modules
 # Execute the task
 
-srun python ./train.py --special_model -o model2.train -b 5 -e 2 -r -d 0.0001 -l 0.001 -t ./databaserelease2/NatureDataset/train/ -v ./databaserelease2/NatureDataset/val/ --build_only
+srun train.sh
--- train.sh
+++ train.sh
@@ -1,1 +1,5 @@
-python ./train.py -i model2 -o model2.train -b 5 -e 2 -r -d 0.0001 -l 0.001 -t ./databaserelease2/NatureDataset/train/ -v ./databaserelease2/NatureDataset/val/
+pip install tensorflow-gpu==2.6
+pip install scikit-learn
+pip install scikit-image
+
+python ./train.py --special_model -o model2.train -b 5 -e 2 -r -d 0.0001 -l 0.001 -t ./databaserelease2/NatureDataset/train/ -v ./databaserelease2/NatureDataset/val/ --build_only
