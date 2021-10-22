1380e6db5028fca136667615154044ddb0635f07
Arash Golchubian <agolchub@koko-login2.hpc.fau.edu>
2021-10-22 16:25:35 -0400

    updated train scripts

--- sb.sh
+++ sb.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#SBATCH --partition=shortq7
+#SBATCH --partition=shortq7-gpu
 #SBATCH  -N 1
 #SBATCH --exclusive
 #SBATCH --mem-per-cpu=16000
 # Load modules, if needed, run staging tasks, etc... Koko Software Modules
 # Execute the task
 
-srun train.sh
+srun sh train.sh
--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ pip install tensorflow-gpu==2.6
 pip install scikit-learn
 pip install scikit-image
 
-python ./train.py --special_model -o model2.train -b 5 -e 2 -r -d 0.0001 -l 0.001 -t ./databaserelease2/NatureDataset/train/ -v ./databaserelease2/NatureDataset/val/ --build_only
+python ./train.py --special_model -o model2.train -b 5 -e 1 -r -d 0.0001 -l 0.001 -t ./databaserelease2/NatureDataset/train/ -v ./databaserelease2/NatureDataset/val/
