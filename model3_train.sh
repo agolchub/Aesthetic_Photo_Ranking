cd6742e6a6fbaeae68de48017c2128b1c5e495d1
Arash Golchubian <arash@pairselfcare.com>
2022-07-10 01:43:26 -0700

    Exclusive is not supported any longer

--- model3_train.sh
+++ model3_train.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #SBATCH --partition=shortq7-gpu
 #SBATCH  -N 1
-#SBATCH --exclusive
-#SBATCH --mem-per-cpu=16000
+#SBATCH --mem-per-cpu=32000
 #SBATCH --gres=gpu:1
 # Load modules, if needed, run staging tasks, etc... Koko Software Modules
 # modeule load shared tensorflow-gpu/2.6.0
