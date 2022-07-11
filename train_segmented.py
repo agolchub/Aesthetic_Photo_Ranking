f389adc4cdf2c0ea4ba36eea370e6e92273c2e18
Arash Golchubian <arash@pairselfcare.com>
2022-07-11 10:45:30 -0700

    Update model 8

--- train_segmented.py
+++ train_segmented.py
@@ -552,49 +552,42 @@ def train(modelin, modelout, imagepath, epochs, batch_size, lr, decay, nesterov,
 
     elif model_design == 8:
         input = layers.Input((WIDTH, HEIGHT, 3))
-        conv2d = new_conv2d(input, 64, (7, 7), strides=(1, 1))
-        maxpool = layers.MaxPooling2D()(conv2d)
-        res1 = new_res_block_collection_v2(3, maxpool, 64)
-        res2 = new_res_block_collection_v2(4, res1, 128, first_strides=(2, 2))
-        res3 = new_res_block_collection_v2(6, res2, 256, first_strides=2)
-        res4 = new_res_block_collection_v2(3, res3, 512, first_strides=2)
+        conv2d = new_conv2d(input, 64, (35, 35), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 128, (17, 17), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 256, (9, 9), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
+        conv2d_1 = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
 
-        res1 = new_conv2d(res1, 128, (3, 3), strides=(2, 2), padding="same")
-        res1 = new_conv2d(res1, 256, (2, 2), strides=(2, 2), padding="same")
-        res1 = new_conv2d(res1, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
+        conv2d = new_conv2d(input, 64, (49, 49), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 128, (23, 23), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 256, (11, 11), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (5, 5), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
+        conv2d_2 = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
 
-        res2 = new_conv2d(res2, 256, (3, 3), strides=(2, 2), padding="same")
-        res2 = new_conv2d(res2, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
+        conv2d = new_conv2d(input, 64, (7, 7), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 128, (5, 5), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 256, (3, 3), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
+        conv2d_3 = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
 
-        res3 = new_conv2d(res3, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
+        conv2d = new_conv2d(input, 64, (128, 128), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 128, (23, 23), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 256, (7, 7), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (5, 5), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (5, 5), strides=(2, 2), padding="same")
+        conv2d_4 = new_conv2d(conv2d, 512, (5, 5), strides=(2, 2), padding="same")
 
-        add = layers.Add()([res1, res2, res3, res4])
+        flat_1 = layers.Flatten()(conv2d_1)
+        flat_2 = layers.Flatten()(conv2d_2)
+        flat_3 = layers.Flatten()(conv2d_3)
+        flat_4 = layers.Flatten()(conv2d_4)
 
-        #
-        # conv2d = new_conv2d(input, 64, (14, 14), strides=(1, 1))
-        # maxpool = layers.MaxPooling2D()(conv2d)
-        # res1 = new_res_block_collection_v2(3, maxpool, 64)
-        # res2 = new_res_block_collection_v2(4, res1, 128, first_strides=(2, 2))
-        # res3 = new_res_block_collection_v2(6, res2, 256, first_strides=2)
-        # res4 = new_res_block_collection_v2(3, res3, 512, first_strides=2)
-        #
-        # res1 = new_conv2d(res1, 128, (3, 3), strides=(2, 2), padding="same")
-        # res1 = new_conv2d(res1, 256, (2, 2), strides=(2, 2), padding="same")
-        # res1 = new_conv2d(res1, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
-        #
-        # res2 = new_conv2d(res2, 256, (3, 3), strides=(2, 2), padding="same")
-        # res2 = new_conv2d(res2, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
-        #
-        # res3 = new_conv2d(res3, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
-        #
-        # concat = layers.Concatenate()([layers.Add()([res1, res2, res3, res4]), add])
+        concat = layers.Concatenate()([flat_1, flat_2, flat_3, flat_4])
 
-
-        res_combined = layers.Activation("relu")(add)
-
-        flat = layers.Flatten()(res_combined)
-
-        output = Dense(1, activation="sigmoid")(flat)
+        output = Dense(1, kernel_initializer="he_uniform", activation="linear")(concat)
         model = models.Model(inputs=input, outputs=output)
 
     else:
