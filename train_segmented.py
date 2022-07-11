f62872b4ae3aa1c81728c4b9fd9e3bc22d1d9fdf
Arash Golchubian <arash@pairselfcare.com>
2022-07-10 19:48:09 -0700

    Add model 7

--- train_segmented.py
+++ train_segmented.py
@@ -517,6 +517,37 @@ def train(modelin, modelout, imagepath, epochs, batch_size, lr, decay, nesterov,
         output = Dense(1, kernel_initializer="he_uniform", activation="linear")(dense)
         model = models.Model(inputs=input, outputs=output)
 
+    elif model_design == 7:
+        input = layers.Input((WIDTH, HEIGHT, 3))
+        conv2d = new_conv2d(input, 64, (35, 35), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 128, (17, 17), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 256, (9, 9), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(1, 1), padding="same")
+        conv2d_1 = new_conv2d(conv2d, 512, (3, 3), strides=(1, 1), padding="same", activation=None, batch_normalization=False)
+
+        conv2d = new_conv2d(input, 64, (49, 49), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 128, (23, 23), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 256, (11, 11), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (5, 5), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(1, 1), padding="same")
+        conv2d_2 = new_conv2d(conv2d, 512, (2, 2), strides=(1, 1), padding="same", activation=None, batch_normalization=False)
+
+        conv2d = new_conv2d(input, 64, (7, 7), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 128, (5, 5), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 256, (3, 3), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2), padding="same")
+        conv2d = new_conv2d(conv2d, 512, (2, 2), strides=(1, 1), padding="same")
+        conv2d_3 = new_conv2d(conv2d, 512, (2, 2), strides=(1, 1), padding="same", activation=None, batch_normalization=False)
+
+        add = layers.Add()([conv2d_1, conv2d_2, conv2d_3])
+        res_combined = layers.Activation("relu")(add)
+
+        flat = layers.Flatten()(res_combined)
+
+        output = Dense(1, kernel_initializer="he_uniform", activation="linear")(flat)
+        model = models.Model(inputs=input, outputs=output)
+
     else:
         model = models.load_model(modelin)
 
