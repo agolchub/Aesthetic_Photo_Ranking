4433834aee5a1a110601a8472fcfe8053a76e721
Arash Golchubian <arash@pairselfcare.com>
2022-07-13 18:47:49 -0700

    Adding model 10

---
+++ model10_train.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH --partition=shortq7-gpu
+#SBATCH  -N 1
+#SBATCH --mem-per-cpu=32000
+#SBATCH --gres=gpu:1
+# Load modules, if needed, run staging tasks, etc... Koko Software Modules
+# modeule load shared tensorflow-gpu/2.6.0
+
+# Execute the task
+conda activate tf2-gpu
+srun python ./train_segmented.py --model_design 10 -o model10.1.train -b 5 --loss bce -e 200 -l 0.1 -t ./databaserelease2/train.csv -v ./databaserelease2/val.csv --width 1024 --height 1024 --outColumn 10
--- train_segmented.py
+++ train_segmented.py
@@ -635,6 +635,33 @@ def train(modelin, modelout, imagepath, epochs, batch_size, lr, decay, nesterov,
 
         output = Dense(5, kernel_initializer="he_uniform", activation="softmax")(concat)
         model = models.Model(inputs=input, outputs=output)
+
+    elif model_design == 10:
+        input = layers.Input((WIDTH, HEIGHT, 3))
+        conv2d = new_conv2d(input, 64, (7, 7), strides=(1, 1))
+        maxpool = layers.MaxPooling2D()(conv2d)
+        res1 = new_res_block_collection_v2(3, maxpool, 64)
+        res2 = new_res_block_collection_v2(4, res1, 128, first_strides=(2, 2))
+        res3 = new_res_block_collection_v2(6, res2, 256, first_strides=2)
+        res4 = new_res_block_collection_v2(3, res3, 512, first_strides=2)
+
+        res1 = new_conv2d(res1, 128, (3, 3), strides=(2, 2), padding="same")
+        res1 = new_conv2d(res1, 256, (2, 2), strides=(2, 2), padding="same")
+        res1 = new_conv2d(res1, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
+
+        res2 = new_conv2d(res2, 256, (3, 3), strides=(2, 2), padding="same")
+        res2 = new_conv2d(res2, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
+
+        res3 = new_conv2d(res3, 512, (2, 2), strides=(2, 2), dropout_rate=0, padding="same", activation=None, batch_normalization=False)
+
+        add = layers.Add()([res1, res2, res3, res4])
+        res_combined = layers.Activation("relu")(add)
+
+        flat = layers.Flatten()(res_combined)
+
+        output = Dense(5, kernel_initializer="he_uniform", activation="softmax")(flat)
+        model = models.Model(inputs=input, outputs=output)
+
     else:
         model = models.load_model(modelin)
 
