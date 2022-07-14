af017267581aba66ba13202d87be8fd9a394b1bc
Arash Golchubian <arash@pairselfcare.com>
2022-07-14 10:32:21 -0700

    change model 11 training to be batched

--- model11_train.sh
+++ model11_train.sh
@@ -9,5 +9,5 @@
 # Execute the task
 conda activate tf2-gpu
 srun python ./train_segmented.py --model_design 11 -o model11.1.train -b 5 --loss bce -e 50 -l 0.1 -t ./databaserelease2/train.csv -v ./databaserelease2/val.csv --width 1024 --height 1024 --outColumn 10
-srun python ./train_segmented.py --model_design 11 -o model11.1.train -b 5 --loss bce -e 100 -l 0.01 -t ./databaserelease2/train.csv -v ./databaserelease2/val.csv --width 1024 --height 1024 --outColumn 10
-srun python ./train_segmented.py --model_design 11 -o model11.1.train -b 5 --loss bce -e 100 -l 0.001 -t ./databaserelease2/train.csv -v ./databaserelease2/val.csv --width 1024 --height 1024 --outColumn 10
+srun python ./train_segmented.py -i model11.1.train -o model11.1.2.train -b 5 --loss bce -e 100 -l 0.01 -t ./databaserelease2/train.csv -v ./databaserelease2/val.csv --width 1024 --height 1024 --outColumn 10
+srun python ./train_segmented.py -i model11.1.2.train -o model11.1.3.train -b 5 --loss bce -e 100 -l 0.001 -t ./databaserelease2/train.csv -v ./databaserelease2/val.csv --width 1024 --height 1024 --outColumn 10
