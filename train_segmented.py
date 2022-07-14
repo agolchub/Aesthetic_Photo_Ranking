f969f3d141d75a9605210d5a09d4b84e73d0fe91
Arash Golchubian <arash@pairselfcare.com>
2022-07-14 03:08:30 -0700

    Adding model 12

--- train_segmented.py
+++ train_segmented.py
@@ -695,6 +695,26 @@ def train(modelin, modelout, imagepath, epochs, batch_size, lr, decay, nesterov,
         output = Dense(5, kernel_initializer="he_uniform", activation="softmax")(concat)
         model = models.Model(inputs=input, outputs=output)
 
+    elif model_design == 12:
+        input = layers.Input((WIDTH, HEIGHT, 3))
+        conv2d = new_conv2d(input, 80, (102, 68), strides=(3, 2))
+        conv2d = new_conv2d(conv2d, 128, (7, 7), strides=(2, 2))
+        conv2d = new_conv2d(conv2d, 256, (3, 3), strides=(1, 1))
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(2, 2))
+        conv2d = new_conv2d(conv2d, 512, (3, 3), strides=(1, 1))
+        conv2d = new_conv2d(conv2d, 256, (3, 3), strides=(2, 2))
+        conv2d = new_conv2d(conv2d, 256, (3, 3), strides=(1, 1))
+        conv2d = new_conv2d(conv2d, 128, (3, 3), strides=(2, 2))
+        conv2d = new_conv2d(conv2d, 128, (3, 3), strides=(1, 1))
+
+        flat = Flatten()(conv2d)
+
+        dense = Dense(128, activation="sigmoid")(flat)
+        dense = Dense(128, activation="sigmoid")(dense)
+
+        output = Dense(5, kernel_initializer="he_uniform", activation="softmax")(dense)
+        model = models.Model(inputs=input, outputs=output)
+
     else:
         model = models.load_model(modelin)
 
