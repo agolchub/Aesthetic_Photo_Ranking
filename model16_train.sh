bb05cb0ecc314c2f195238204243d6d2445f1621
Arash Golchubian <arash@pairselfcare.com>
2022-07-18 08:21:00 -0700

    Smaller test set

--- model16_train.sh
+++ model16_train.sh
@@ -8,4 +8,4 @@
 
 # Execute the task
 conda activate tf2-gpu
-srun python ./train_segmented.py --model_design 16 -o model16.3.train -b 5 --momentum 0.01 --loss mse -e 1000 -l 0.01 -d 0.01 --patience 50 -t ./databaserelease2/train2.csv -v ./databaserelease2/test2.csv --width 1024 --height 680 --outColumn 2 --reload_checkpoint_between_rates
+srun python ./train_segmented.py --model_design 16 -o model16.3.train -b 5 --momentum 0.01 --loss mse -e 1000 -l 0.01 -d 0.01 --patience 50 -t ./databaserelease2/train2.csv -v ./databaserelease2/val2.csv --width 1024 --height 680 --outColumn 2 --reload_checkpoint_between_rates
