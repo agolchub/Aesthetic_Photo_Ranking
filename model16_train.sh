92dfb522ded59001722e7a682113ef07fa4fdde1
Arash Golchubian <arash@pairselfcare.com>
2022-07-18 09:11:47 -0700

    Smaller training set

--- model16_train.sh
+++ model16_train.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH --partition=shortq7-gpu
 #SBATCH  -N 1
-#SBATCH --mem-per-cpu=64000M
+#SBATCH --mem-per-cpu=16000M
 #SBATCH --gres=gpu:1
 # Load modules, if needed, run staging tasks, etc... Koko Software Modules
 # modeule load shared tensorflow-gpu/2.6.0
