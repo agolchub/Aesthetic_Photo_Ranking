047f235cb04650fcf925a52af99545923b70454d
Arash Golchubian <arash@pairselfcare.com>
2022-07-18 19:30:21 -0700

    Extra training to intentionally cause overtraining.

--- model17_train.sh
+++ model17_train.sh
@@ -7,5 +7,6 @@
 # modeule load shared tensorflow-gpu/2.6.0
 
 # Execute the task
-conda activate tf2-gpu
-srun python ./train_segmented.py --model_design 17 -o model17.2.train -b 5 --momentum 0.01 --loss mse -e 1000 -l 0.1 -d 0.01 --patience 50 -t ./databaserelease2/train2.csv -v ./databaserelease2/val2.csv --width 1024 --height 680 --outColumn 2 --reload_checkpoint_between_rates
+#conda activate tf2-gpu
+#srun python ./train_segmented.py --model_design 17 -o model17.2.train -b 5 --momentum 0.01 --loss mse -e 1000 -l 0.1 -d 0.01 --patience 50 -t ./databaserelease2/train2.csv -v ./databaserelease2/val2.csv --width 1024 --height 680 --outColumn 2 --reload_checkpoint_between_rates
+srun python ./train_segmented.py -i model17.2.train/model -o model17.3.train -b 5 --momentum 0.01 --loss mse -e 100 -l 0.01 -d 0.01 --patience 100 -t ./databaserelease2/train2.csv -v ./databaserelease2/val2.csv --width 1024 --height 680 --outColumn 2 --reload_checkpoint_between_rates
